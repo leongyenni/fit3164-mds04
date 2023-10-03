ecb64f6a7242e13851080a4ad7f17cd098099f3e
qlai0003 <qlai0003@student.monash.edu>
2023-10-03 16:51:07 +0800

    Modify model code

--- server/nbeats_revin_model.py
+++ server/nbeats_revin_model.py
@@ -24,51 +24,57 @@ def preprocess():
 
     df2 = df2[['Open','High','Low','Close','Adj Close']]
 
+    # Extract the last date in the index
+    last_date = df2.index[-1]
+
+    # Add one day to the date and set the time to 09:30:00-04:00
+    new_date = pd.Timestamp(last_date.date() + pd.Timedelta(days=1), tz='US/Eastern').replace(hour=9, minute=30)
+
+    # Append the new date to the DataFrame with NaN values for all columns
+    df2.loc[new_date] = [pd.NA] * len(df2.columns)
+
+    df2.tail(10)
+
 
     HORIZON=7
-    WINDOW_SIZE=49
+    WINDOW_SIZE=7
 
     # Make a copy of the Bitcoin historical data with block reward feature
     df_windowed2 = df2.copy()
 
     # Add windowed columns
-    for i in range(7): # Shift values for each step in WINDOW_SIZE
-        df_windowed2[f"Open-{7-i}"] = df_windowed2["Open"].shift(periods=i+1)
-        df_windowed2[f"High-{7-i}"] = df_windowed2["High"].shift(periods=i+1)
-        df_windowed2[f"Low-{7-i}"] = df_windowed2["Low"].shift(periods=i+1)
-        df_windowed2[f"Close-{7-i}"] = df_windowed2["Close"].shift(periods=i+1)
-        df_windowed2[f"Adj Close-{7-i}"] = df_windowed2["Adj Close"].shift(periods=i+1)
+    for i in range(WINDOW_SIZE): # Shift values for each step in WINDOW_SIZE
+      df_windowed2[f"Open-{WINDOW_SIZE-i}"] = df_windowed2["Open"].shift(periods=i+1)
+      df_windowed2[f"High-{WINDOW_SIZE-i}"] = df_windowed2["High"].shift(periods=i+1)
+      df_windowed2[f"Low-{WINDOW_SIZE-i}"] = df_windowed2["Low"].shift(periods=i+1)
+      df_windowed2[f"Close-{WINDOW_SIZE-i}"] = df_windowed2["Close"].shift(periods=i+1)
+      df_windowed2[f"Adj Close-{WINDOW_SIZE-i}"] = df_windowed2["Adj Close"].shift(periods=i+1)
 
 
-    feature_cols2 = ['Open', 'High', 'Low', 'Adj Close']+[f"{col}-{i}" for i in range(7, 0, -1) for col in ['Open', 'High', 'Low', 'Close', 'Adj Close']]
-    X2 = df_windowed2.dropna()[feature_cols2]
-    y2 = df_windowed2.dropna()['Close']
-    X2, y2
+    # Step 1: Filter out columns with -7 to -1 suffix
+    suffixes_to_check = [f"-{i}" for i in range(7, 0, -1)]
+    columns_to_check_for_nan = [col for col in df_windowed2.columns if any(suffix in col for suffix in suffixes_to_check)]
+
+    # Step 2: Drop rows only if there's a NaN in columns with the -7 to -1 suffix
+    indices_to_keep = df_windowed2.dropna(subset=columns_to_check_for_nan).index
+    df_filtered = df_windowed2.loc[indices_to_keep]
+
+    # Step 3: Drop columns 'Open', 'High', 'Low', and 'Adj Close' without the -7 to -1 suffixes
+    cols_to_exclude = ['Open', 'High', 'Low', 'Adj Close']
+    cols_to_drop = [col for col in df_filtered.columns if col in cols_to_exclude and not any(suffix in col for suffix in suffixes_to_check)]
+    df_filtered = df_filtered.drop(columns=cols_to_drop)
+
+    # Separating into X2 and y2
+    X2 = df_filtered.drop(columns=['Close'])
+    y2_withClose = df_filtered['Close']
 
 
-    # Ensure timestamps you're using for indexing have timezone information.
-    # start_train = pd.Timestamp('2022-10-01').tz_localize('US/Eastern')
-    # end_train = pd.Timestamp('2023-04-30').tz_localize('US/Eastern')
+    start_test = pd.Timestamp('2023-09-07').tz_localize('US/Eastern')
+    end_test = pd.Timestamp('2023-09-17').tz_localize('US/Eastern')
 
-    # start_val = pd.Timestamp('2023-05-01').tz_localize('US/Eastern')
-    # end_val = pd.Timestamp('2023-05-31').tz_localize('US/Eastern')
-
-    start_test = pd.Timestamp('2023-08-31').tz_localize('US/Eastern')
-    end_test = pd.Timestamp('2023-09-12').tz_localize('US/Eastern')
-
-    # start_pred = pd.Timestamp('2023-09-12').tz_localize('US/Eastern')
-    # end_pred = pd.Timestamp('2023-09-13').tz_localize('US/Eastern')
-
-    X_test2, y_test2 = X2.loc[start_test:end_test], y2.loc[start_test:end_test]
-
-    BATCH_SIZE = 1024
-
-    test_dataset_norm2 = (tf.data.Dataset
-                    .from_tensor_slices((X_test2, y_test2))
-                    .batch(BATCH_SIZE)
-                    .prefetch(buffer_size=tf.data.AUTOTUNE))
+    X_test2, y_test2_withClose = X2.loc[start_test:end_test], y2_withClose.loc[start_test:end_test]
     
-    return test_dataset_norm2
+    return X_test2
 
 
 def make_preds(model, input_data):
@@ -87,14 +93,14 @@ def make_preds(model, input_data):
 
 
 # if __name__=="__main__":
-test_dataset_norm2=preprocess()
+X_test2=preprocess()
 
 loaded_w_revin_model=tf.keras.models.load_model("./nbeats_revin_model")
 
-model_w_revin_preds = make_preds(loaded_w_revin_model, test_dataset_norm2)
+model_w_revin_preds = make_preds(loaded_w_revin_model, X_test2)
 
-model_w_revin_preds_reshaped = model_w_revin_preds[:, -1]  # Take the last column
-model_w_revin_preds_reshaped = tf.expand_dims(model_w_revin_preds_reshaped, axis=-1)  
-model_w_revin_preds_lastcol=model_w_revin_preds_reshaped[-7:]
+model_w_revin_preds_lastcol = model_w_revin_preds[:, -1]  # Take the last column
+model_w_revin_preds_lastcol = tf.expand_dims(model_w_revin_preds_lastcol, axis=-1)  # Expand dimensions to shape (494, 1)
+model_w_revin_preds_lastcol_seven=model_w_revin_preds_lastcol[-7:]
 
-print(model_w_revin_preds_lastcol)
+print(model_w_revin_preds_lastcol_seven)
--- server/nbeats_revin_model/fingerprint.pb
+++ server/nbeats_revin_model/fingerprint.pb
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2f918650ad956b33b688e77246f14618f2a173dfcb415881a8359ec6442779f7
-size 58
+ƒß±ç¨¡…‘Ùµö¬â·œ‰×Ä°ÂâÏ²ÂğÇ ƒƒÕŒ²×‚°è(£ôÅÛÀ¤ºï÷2
