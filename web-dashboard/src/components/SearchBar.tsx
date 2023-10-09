2b8075fbfc82030e37a92c1abd185049a179c32b
Leong Yen Ni <leongyenni@gmail.com>
2023-10-09 19:02:56 +0800

    Fixed search bar bug - Prevent user submit invalid input.

--- web-dashboard/src/components/SearchBar.tsx
+++ web-dashboard/src/components/SearchBar.tsx
@@ -21,16 +21,15 @@ const Searchbar: React.FC<SearchBarProps> = ({ className }) => {
     };
 
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter' && tickerSymbols.length === 0) {
-            // Prevent "Enter" key event when tickerSymbols.length is 0
-            e.preventDefault();
-        } else if (e.key === 'Enter') {
-            // Handle "Enter" key event when tickerSymbols.length > 0
-            const inputElement = e.target as HTMLInputElement;
-            handleClickValue(inputElement.value.toUpperCase());
-        }
+         if (e.key === 'Enter' && tickerSymbols.length === 0) {
+             e.preventDefault();
+         } else if (e.key === 'Enter') {
+             e.preventDefault();
+             const inputElement = e.target as HTMLInputElement;
+             handleClickValue(inputElement.value.toUpperCase());
+         }
     };
-    
+
     const handleClickValue = (tickerSymbol: string) => {
         setOpen(false);
         setInputVal(tickerSymbol);
@@ -76,16 +75,7 @@ const Searchbar: React.FC<SearchBarProps> = ({ className }) => {
                         onChange={(e) => {
                             handleChange(e.target.value.toUpperCase());
                         }}
-                        onKeyDown={(e) => {
-                            if (e.key === 'Enter') {
-                                e.preventDefault();
-                                const inputElement =
-                                    e.target as HTMLInputElement;
-                                handleClickValue(
-                                    inputElement.value.toUpperCase()
-                                );
-                            }
-                        }}
+                        onKeyDown={handleKeyDown}
                     />
                 </div>
                 {open && (
@@ -94,7 +84,9 @@ const Searchbar: React.FC<SearchBarProps> = ({ className }) => {
                         className="search-bar absolute top-3 mt-12 w-3/5 max-h-[240px] z-50 bg-slate-950 overflow-y-scroll rounded-lg shadow-md shadow-zinc-950"
                     >
                         {tickerSymbols.length === 0 && inputVal.length > 1 ? (
-                            <p className="text-white p-2">No results found.</p>
+                            <p className="text-white p-2 tracking-wider">
+                                No results found.
+                            </p>
                         ) : (
                             <ul>
                                 {tickerSymbols.map((tickerSymbol) => (
