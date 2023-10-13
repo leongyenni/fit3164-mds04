951b8c0e2541d7e6cd742f507028319099e97d03
qlai0003 <qlai0003@student.monash.edu>
2023-10-13 21:01:49 +0800

    Add install libraries.bat file

--- windows_start_app.bat
+++ windows_start_app.bat
@@ -1,3 +1,18 @@
-start /b cmd /c "cd web-dashboard && npm run dev"
-start /b cmd /c "cd server && npm start"
-cd server && python flask_server.py
+@echo off
+echo Installing npm packages in the root directory...
+cmd /c npm install
+
+cd web-dashboard
+echo Installing npm packages for web-dashboard...
+cmd /c npm install
+
+echo Start dashboard...
+start /b cmd /c "npm run dev"
+
+cd ..
+cd server
+echo Start servers...
+start /b cmd /c "npm start"
+python flask_server.py
+
+echo Run completed.
